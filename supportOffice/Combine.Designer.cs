dc2800fb9f1577c0603a0c596e6292e371987dfc
BẢO HÀ HOÀI <20200135@student.hcmus.edu.vn>
2023-07-21 22:42:27 +0700

    fix: interface

--- supportOffice/Combine.Designer.cs
+++ supportOffice/Combine.Designer.cs
@@ -54,36 +54,38 @@
             this.panelCombine.Controls.Add(this.optionP);
             this.panelCombine.Controls.Add(this.optionW);
             this.panelCombine.Controls.Add(this.label2);
-            this.panelCombine.Location = new System.Drawing.Point(189, 49);
-            this.panelCombine.Margin = new System.Windows.Forms.Padding(2);
+            this.panelCombine.Location = new System.Drawing.Point(134, 45);
+            this.panelCombine.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelCombine.Name = "panelCombine";
-            this.panelCombine.Size = new System.Drawing.Size(642, 537);
+            this.panelCombine.Size = new System.Drawing.Size(834, 606);
             this.panelCombine.TabIndex = 11;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(263, 346);
+            this.label3.Location = new System.Drawing.Point(351, 350);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.Size = new System.Drawing.Size(141, 16);
             this.label3.TabIndex = 13;
             this.label3.Text = "Address Folder Output";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(263, 202);
+            this.label1.Location = new System.Drawing.Point(351, 249);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.Size = new System.Drawing.Size(121, 16);
             this.label1.TabIndex = 12;
             this.label1.Text = "Address Files Input";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(278, 385);
-            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Location = new System.Drawing.Point(371, 398);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(56, 19);
+            this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 11;
             this.button5.Text = "Browser";
             this.button5.UseVisualStyleBackColor = true;
@@ -91,18 +93,18 @@
             // 
             // Savet3
             // 
-            this.Savet3.Location = new System.Drawing.Point(189, 361);
-            this.Savet3.Margin = new System.Windows.Forms.Padding(2);
+            this.Savet3.Location = new System.Drawing.Point(252, 368);
+            this.Savet3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Savet3.Name = "Savet3";
-            this.Savet3.Size = new System.Drawing.Size(271, 20);
+            this.Savet3.Size = new System.Drawing.Size(360, 22);
             this.Savet3.TabIndex = 10;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(239, 443);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Location = new System.Drawing.Point(320, 486);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 58);
+            this.button3.Size = new System.Drawing.Size(181, 71);
             this.button3.TabIndex = 9;
             this.button3.Text = "Combine";
             this.button3.UseVisualStyleBackColor = true;
@@ -110,10 +112,10 @@
             // 
             // inputcb1
             // 
-            this.inputcb1.Location = new System.Drawing.Point(278, 241);
-            this.inputcb1.Margin = new System.Windows.Forms.Padding(2);
+            this.inputcb1.Location = new System.Drawing.Point(371, 297);
+            this.inputcb1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.inputcb1.Name = "inputcb1";
-            this.inputcb1.Size = new System.Drawing.Size(56, 19);
+            this.inputcb1.Size = new System.Drawing.Size(75, 23);
             this.inputcb1.TabIndex = 7;
             this.inputcb1.Text = "Browser";
             this.inputcb1.UseVisualStyleBackColor = true;
@@ -121,19 +123,19 @@
             // 
             // listFilesText
             // 
-            this.listFilesText.Location = new System.Drawing.Point(189, 217);
-            this.listFilesText.Margin = new System.Windows.Forms.Padding(2);
+            this.listFilesText.Location = new System.Drawing.Point(252, 267);
+            this.listFilesText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listFilesText.Name = "listFilesText";
-            this.listFilesText.Size = new System.Drawing.Size(271, 20);
+            this.listFilesText.Size = new System.Drawing.Size(360, 22);
             this.listFilesText.TabIndex = 5;
             // 
             // optionP
             // 
             this.optionP.AutoSize = true;
-            this.optionP.Location = new System.Drawing.Point(442, 132);
-            this.optionP.Margin = new System.Windows.Forms.Padding(2);
+            this.optionP.Location = new System.Drawing.Point(589, 162);
+            this.optionP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.optionP.Name = "optionP";
-            this.optionP.Size = new System.Drawing.Size(46, 17);
+            this.optionP.Size = new System.Drawing.Size(55, 20);
             this.optionP.TabIndex = 2;
             this.optionP.TabStop = true;
             this.optionP.Text = "PDF";
@@ -142,10 +144,10 @@
             // optionW
             // 
             this.optionW.AutoSize = true;
-            this.optionW.Location = new System.Drawing.Point(179, 132);
-            this.optionW.Margin = new System.Windows.Forms.Padding(2);
+            this.optionW.Location = new System.Drawing.Point(239, 162);
+            this.optionW.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.optionW.Name = "optionW";
-            this.optionW.Size = new System.Drawing.Size(51, 17);
+            this.optionW.Size = new System.Drawing.Size(61, 20);
             this.optionW.TabIndex = 1;
             this.optionW.TabStop = true;
             this.optionW.Text = "Word";
@@ -155,19 +157,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(224, 60);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(299, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 37);
+            this.label2.Size = new System.Drawing.Size(193, 46);
             this.label2.TabIndex = 0;
             this.label2.Text = "Combine";
             // 
             // Combine
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(951, 660);
+            this.ClientSize = new System.Drawing.Size(1145, 696);
             this.Controls.Add(this.panelCombine);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Combine";
             this.Text = "Combine";
             this.panelCombine.ResumeLayout(false);
--- supportOffice/Form1.Designer.cs
+++ supportOffice/Form1.Designer.cs
@@ -42,19 +42,21 @@
             // 
             // Convert
             // 
-            this.Convert.Location = new System.Drawing.Point(32, 56);
+            this.Convert.Location = new System.Drawing.Point(43, 69);
+            this.Convert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Convert.Name = "Convert";
-            this.Convert.Size = new System.Drawing.Size(155, 47);
+            this.Convert.Size = new System.Drawing.Size(207, 58);
             this.Convert.TabIndex = 0;
-            this.Convert.Text = "Convert File (Word, PDF)";
+            this.Convert.Text = "Convert File";
             this.Convert.UseVisualStyleBackColor = true;
             this.Convert.Click += new System.EventHandler(this.convertPage_Click);
             // 
             // Combine
             // 
-            this.Combine.Location = new System.Drawing.Point(32, 121);
+            this.Combine.Location = new System.Drawing.Point(43, 149);
+            this.Combine.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Combine.Name = "Combine";
-            this.Combine.Size = new System.Drawing.Size(155, 47);
+            this.Combine.Size = new System.Drawing.Size(207, 58);
             this.Combine.TabIndex = 1;
             this.Combine.Text = "Combine File";
             this.Combine.UseVisualStyleBackColor = true;
@@ -62,9 +64,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(32, 185);
+            this.button3.Location = new System.Drawing.Point(43, 228);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(155, 47);
+            this.button3.Size = new System.Drawing.Size(207, 58);
             this.button3.TabIndex = 2;
             this.button3.Text = "Create File (Word, Text)";
             this.button3.UseVisualStyleBackColor = true;
@@ -72,9 +75,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(32, 250);
+            this.button4.Location = new System.Drawing.Point(43, 308);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(155, 47);
+            this.button4.Size = new System.Drawing.Size(207, 58);
             this.button4.TabIndex = 3;
             this.button4.Text = "Create File Excel";
             this.button4.UseVisualStyleBackColor = true;
@@ -82,9 +86,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(32, 313);
+            this.button5.Location = new System.Drawing.Point(43, 385);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(155, 47);
+            this.button5.Size = new System.Drawing.Size(207, 58);
             this.button5.TabIndex = 4;
             this.button5.Text = "Rename File";
             this.button5.UseVisualStyleBackColor = true;
@@ -93,38 +98,41 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(336, 19);
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 24);
+            this.label1.Size = new System.Drawing.Size(228, 28);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Support Office Tools";
+            this.label1.Text = "SUPPORT OFFICE";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(233, 56);
+            this.pictureBox1.Location = new System.Drawing.Point(311, 69);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(498, 368);
+            this.pictureBox1.Size = new System.Drawing.Size(664, 453);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(32, 377);
+            this.button1.Location = new System.Drawing.Point(43, 464);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 47);
+            this.button1.Size = new System.Drawing.Size(207, 58);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Split File (PDF)";
+            this.button1.Text = "Split File";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.splitFilePage_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
@@ -133,8 +141,9 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.Combine);
             this.Controls.Add(this.Convert);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Support Office";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
